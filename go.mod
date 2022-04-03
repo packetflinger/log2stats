adec2383a3db568fc1e8fd4e442cafcdaddcae2d
Joe Reid <joe@joereid.com>
2022-04-03 14:11:34 -0400

    Initial, just reading the file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/packetflinger/q2logparser
+
+go 1.17
---
+++ log2stats.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
